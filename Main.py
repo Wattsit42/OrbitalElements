Turns out everything worked fine, my scaling of the axis was just wrong and I was using a crazy timescale for looking at the Earth's orbit, I was essentially just plotting it a year in the future each time which understandably made it just appear in the same place over and over again.

Main.py
@@ -14,26 +14,23 @@ class Planet:
 
     def __init__(self,orbital_param):
         self.orbital_param = orbital_param
-        self.n = 2*np.pi/(self.orbital_param[0]**(3/2))
-        self.lamda_bar0 = rad_convert(self.orbital_param[1])
-        self.lamda = 0.0
-        self.lamda_bar = 0.0
         self.pos = [0.0,0.0,0.0]
-        self.set_pos(0)
+        self.n = 2*np.pi/(self.orbital_param[0]**(3/2)) #I define n in this scope as I don't want to be calculating it every time I run the set_pos function
 
     def set_pos(self,t):
+        a = self.orbital_param[0]
+        lamda0 = rad_convert(self.orbital_param[1])
         e = self.orbital_param[2]
-        omega_bar = rad_convert(self.orbital_param[4])
-        omega = rad_convert(self.orbital_param[5])
-        self.lamda_bar = self.lamda_bar0 + self.n * t
-        r = self.orbital_param[0]*(1-e*np.cos(self.lamda_bar-omega_bar))
-        self.lamda = self.lamda_bar + 2*e*np.cos(self.lamda_bar-omega_bar)
-        beta = self.orbital_param[3]*np.sin(self.lamda_bar-omega)
-        self.pos[0] = r*np.cos(self.lamda)
-        self.pos[1] = r*np.sin(self.lamda)
-        self.pos[2] = r*rad_convert(self.orbital_param[3])*np.sin(self.lamda-omega)
+        w_bar = rad_convert(self.orbital_param[4])
+        lamda_bar = lamda0 + self.n * t
+        r = a*(1-e*np.cos(lamda_bar - w_bar))
+        lamda = lamda_bar + 2*e*np.sin(lamda_bar - w_bar)
+        self.pos[0] = r*np.cos(lamda)
+        self.pos[1] = r*np.sin(lamda)
+        self.pos[2] = r*rad_convert(self.orbital_param[3])*np.sin(lamda_bar-rad_convert(self.orbital_param[5]))
 
 planetary_dataset = [[0.3871,252.25,0.20564,7.006,77.46,48.34,0.241,1.659*(10**-7)],[0.7233,181.98,0.00676,3.398,131.77,76.67,0.615,2.447*(10**-6)],[1,100.47,0.01673,0,102.93,0,1,3.039*(10**-6)],[1.5237,355.43,0.09337,1.852,336.08,49.71,1.881,3.226*(10**-7)],[5.2025,34.33,0.04854,1.299,14.27,100.29,11.87,9.542*(10**-4)],[9.5415,50.08,0.05551,2.494,92.86,113.64,29.47,2.857*(10**-4)],[19.188,314.20,0.04686,0.773,172.43,73.96,84.05,4.353*(10**-5)],[30.070,304.22,0.00895,1.770,46.68,131.79,164.9,5.165*(10**-5)]]
+#[0.3871,252.25,0.20564,7.006,77.46,48.34,0.241,1.659*(10**-7)],[0.7233,181.98,0.00676,3.398,131.77,76.67,0.615,2.447*(10**-6)],[1,100.47,0.01673,0,102.93,0,1,3.039*(10**-6)],[1.5237,355.43,0.09337,1.852,336.08,49.71,1.881,3.226*(10**-7)],[5.2025,34.33,0.04854,1.299,14.27,100.29,11.87,9.542*(10**-4)],[9.5415,50.08,0.05551,2.494,92.86,113.64,29.47,2.857*(10**-4)],[19.188,314.20,0.04686,0.773,172.43,73.96,84.05,4.353*(10**-5)],[30.070,304.22,0.00895,1.770,46.68,131.79,164.9,5.165*(10**-5)]
 #version
 Planets = []
 
@@ -43,7 +40,8 @@ for i in range(len(planetary_dataset)):
 
 fig = plt.figure()
 ax = plt.axes(projection='3d')
-for t in range(0,1000):
+time = np.linspace(0,164,100)
+for t in time:
     #ax.clear()
     positions = [[],[],[]]
 
@@ -56,5 +54,5 @@ for t in range(0,1000):
     ax.scatter(positions[0],positions[1],positions[2],)
     ax.set_xlim([-35,35])
     ax.set_ylim([-35,35])
-    ax.set_zlim([-1.2,1.2])
+    ax.set_zlim([-35,35])
     fig.show()